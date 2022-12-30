48206083f9b507c5340df7f9c6c9d0727d12f741
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-30 01:57:28 +0100

    Add styled components

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,23 @@
 import ReactDOM from "react-dom/client";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: white;
+  color: palevioletred;
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  &:hover {
+    color: red;
+    border-color: red;
+    cursor: pointer;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <button>PANZER VOR!</button>
+  <Button>PANZER VOR!</Button>
 );
