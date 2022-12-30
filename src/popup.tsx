ec46e6432fedf371fc1ab14ae8e75fa44da1fe3c
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-30 02:04:53 +0100

    Move Button styled to component to styled folder

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,22 +1,5 @@
 import ReactDOM from "react-dom/client";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: white;
-  color: palevioletred;
-
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-
-  &:hover {
-    color: red;
-    border-color: red;
-    cursor: pointer;
-  }
-`;
+import { Button } from "./styled";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Button>PANZER VOR!</Button>
